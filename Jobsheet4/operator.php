680124b2858dfa19653a009405ea4871ab489abb
rifat syamlan <rifatsyamlan78@gmail.com>
2025-09-24 13:49:30 +0700

    commit

---
+++ Jobsheet4/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasiTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil Penjumlahan: $a + $b = $hasiTambah <br>";
+echo "Hasil Pengurangan: $a - $b = $hasilKurang <br>";
+echo "Hasil Perkalian: $a * $b = $hasilKali <br>";
+echo "Hasil Pembagian: $a / $b = $hasilBagi <br>";
+echo "Hasil Sisa Bagi: $a % $b = $sisaBagi <br>";
+echo "Hasil Pangkat: $a ** $b = $pangkat <br>";
+?>
