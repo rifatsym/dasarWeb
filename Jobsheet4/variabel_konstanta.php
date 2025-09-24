31b330e62aacc3a9f25d9cf06a9027b349a5e9a7
rifat syamlan <rifatsyamlan78@gmail.com>
2025-09-24 12:55:07 +0700

    commit

--- Jobsheet4/variabel_konstanta.php
+++ Jobsheet4/variabel_konstanta.php
@@ -6,11 +6,11 @@ echo "Hasil Penjumlahan: $angka1 dan $angka2 adalah $hasil.";
 
 $benar = true;
 $salah = false;
-echo "Variabel benar: $benar, Variabel salah: $salah";
+echo "<br>Variabel benar: $benar, Variabel salah: $salah";
 
 define("NAMA_SITUS", "WebsiteKu.com");
 define("TAHUN_PENDIRIAN", 2023);
 
-echo "Selamat datang di " . NAMA_SITUS . ", situs yang didirikan pada tahun "
+echo "<br>Selamat datang di " . NAMA_SITUS . ", situs yang didirikan pada tahun "
 . TAHUN_PENDIRIAN . ".";
 ?>
