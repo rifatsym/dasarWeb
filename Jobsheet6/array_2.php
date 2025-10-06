998ef02e88a75b7fa98393f9afb0808bf82d7e52
rifat syamlan <rifatsyamlan78@gmail.com>
2025-10-06 15:34:55 +0700

    commit

---
+++ Jobsheet6/array_2.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $dosen = [
+            'Nama' => 'Elok',
+            'domisili' => 'Malang',
+            'jenis_kelamin' => 'Perempuan'
+        ];
+
+        echo "Nama: {$dosen ['nama']} <br>";
+        echo "Domisili: {$dosen ['domisili']} <br>";
+        echo "Jenis Kelamin: {$dosen ['jenis_kelamin']} <br>";
+        ?>
+    </body>
+</html>
