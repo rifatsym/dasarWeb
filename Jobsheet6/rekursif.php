aae420381c00307a05a223dec1e887a40e2ed69a
rifat syamlan <rifatsyamlan78@gmail.com>
2025-10-06 20:21:29 +0700

    commit

--- Jobsheet6/rekursif.php
+++ Jobsheet6/rekursif.php
@@ -1,10 +1,45 @@
 <?php
-function tampilkanAngka (int $jumlah, int $indeks = 1){
-    echo "Perulangan ke-{$indeks} <br>";
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ],
+];
 
-    if ($indeks < $jumlah) {
-        tampilkanAngka($jumlah, $indeks + 1);
+function tampilkanMenuBertingkat(array $menu) {
+    echo "<ul>";
+    foreach ($menu as $key => $item) {
+        echo "<li>{$item['nama']}</li>";
     }
+    echo "</ul>";
 }
-tampilkanAngka(20);
+
+tampilkanMenuBertingkat($menu);
 ?>
