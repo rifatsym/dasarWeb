83e140a09852408ed5acb3c34164ae1c8a9effd7
rifat syamlan <rifatsyamlan78@gmail.com>
2025-10-06 21:20:43 +0700

    commit

--- Jobsheet6/variabel.php
+++ Jobsheet6/variabel.php
@@ -8,7 +8,7 @@
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // collect value of input field
-        $name = $_POST['fname'];
+        $name = $_REQUEST['fname'];
         if (empty($name)) {
             echo "Name is empty";
         } else {
