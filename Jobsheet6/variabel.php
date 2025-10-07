8315bbc74b939ba9e3c0b688bda4c60ebdc27982
rifat syamlan <rifatsyamlan78@gmail.com>
2025-10-07 08:20:27 +0700

    commit

--- Jobsheet6/variabel.php
+++ Jobsheet6/variabel.php
@@ -1,20 +1,11 @@
-<html>
-<body>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-        Name: <input type="text" name="fname">
-        <input type="submit">
-    </form>
+<?php
+$x = 75;
+$y = 25;
 
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // collect value of input field
-        $name = $_REQUEST['fname'];
-        if (empty($name)) {
-            echo "Name is empty";
-        } else {
-            echo $name;
-        }
-    }
-    ?>
-</body>
-</html>
+function addition() {
+    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+}
+
+addition();
+echo $z;
+?>
